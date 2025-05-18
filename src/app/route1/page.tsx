b8d9c797b81152f088fc8d9a29084e147a19b10e
Ron <ronald.prato@unet.edu.ve>
2025-05-18 04:00:48 -0500

    test 1

---
+++ src/app/route1/page.tsx
@@ -0,0 +1,9 @@
+export default function Route1() {
+  return (
+    <div>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <h1>Route 1</h1>
+      </main>
+    </div>
+  );
+}
